ac3978acb6e1a23a4966be73d446a5dc5e2d698c
Yandex Praktikum <praktikum@yandex.ru>
2023-12-22 16:46:10 +0300

    Initial Commit

---
+++ Cunts12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cunts12
+//
+//  Created by  Admin on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
